1fe6f46709d83416caec8bf1723606d5606c9c84
BooZDev <trantuananhbdev@gmail.com>
2024-10-15 17:47:56 +0700

    first time

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.game.itgame {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.game.itgame to javafx.fxml;
+    exports com.game.itgame;
+}
