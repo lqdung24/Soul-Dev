5ab0bcf560463b21baf8b5e39f54ac480b17e960
Dung le <dunglevpr@gmail.com>
2024-10-23 15:16:30 +0700

    foundation: move player, get map, check collision

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,12 @@ module com.game.itgame {
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
+    requires java.desktop;
+    requires annotations;
 
-    opens com.game.itgame to javafx.fxml;
-    exports com.game.itgame;
+    exports com.game.itgame.test;
+    exports com.game.itgame.main;
+    opens com.game.itgame.main to javafx.fxml;
+    exports com.game.itgame.controller;
+    opens com.game.itgame.controller to javafx.fxml;
 }
