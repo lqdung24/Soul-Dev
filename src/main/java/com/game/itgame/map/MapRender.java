0b62f5a6dc87f31872eea9beec5c92ed232c91d3
BooZDev <trantuananhbdev@gmail.com>
2024-10-25 20:05:05 +0700

    fix

--- src/main/java/com/game/itgame/map/MapRender.java
+++ src/main/java/com/game/itgame/map/MapRender.java
@@ -10,21 +10,25 @@ import java.util.Objects;
 
 public class MapRender extends MapMove implements Map {
     private double mapFrameSize = 30;
-    private int startX = 20;
-    private int startY = 16;
+    private int startX = 2;
+    private int startY = 2;
 
     public void mapRender(GraphicsContext ctx, Player player, KeyHandle key) {
         mapMove(player, key);
         double mapX = ctx.getCanvas().getWidth() / 2 - (startX * mapFrameSize + mapFrameSize / 2) -x ;
         double mapY = ctx.getCanvas().getHeight() / 2 - (startY * mapFrameSize + mapFrameSize / 2) -y ;
 
+//        Lưu trạng thaái ban đầu của canvas.
         ctx.save();
+//        Di chuyển tới vị trí đầu tiên của map.
         ctx.translate(mapX, mapY);
+//        Vẽ map.
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[i].length; j++) {
                 ctx.drawImage(mapImageIndex[map[i][j]], 0, 0, 512, 512, j * 30, i * 30, 30, 30);
             }
         }
+//        Trả về trạng thái ban đầu của canvas.
         ctx.restore();
     }
 
